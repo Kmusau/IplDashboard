b49c30194e2ae506fba4a4008632e79b09a2a26c
kevo <kevomusa6565@gmail.com>
2022-01-07 13:11:42 +0300

    First commit - prepared data using spring batch and saved it in a HSQL database

---
+++ src/main/java/com/kmusau/Ipldashboard/IpldashboardApplication.java
@@ -0,0 +1,13 @@
+package com.kmusau.Ipldashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpldashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpldashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kmusau/Ipldashboard/IpldashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmusau.Ipldashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpldashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
